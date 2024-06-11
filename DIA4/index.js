e250528f50a127f6b26cb8c9404df0279b610b2c
Deivide Costa <deividec51@gmail.com>
2024-06-11 16:07:47 -0300

    Manipulando operadores ||

--- DIA4/index.js
+++ DIA4/index.js
@@ -1,4 +1,4 @@
-let fome = prompt('Você está com fome ou sem fome???')
+/*let fome = prompt('Você está com fome ou sem fome???')
 let dinheiro = prompt('Você está com dinheiro ou sem dinheiro???')
 let restaurante = prompt('está fechado ou aberto???')
 
@@ -11,4 +11,20 @@ else if(fome==="com fome" && dinheiro === "com dinheiro" && restaurante === "fec
 
 else if(fome === "coom fome" && dinheiro === "com dinheiro" && restaurante === "aberto"){
     console.log("hoje o jantar será no seu restaurante preferido!!")
+}*/
+
+let nome = prompt('Qual o seu nome?')
+let idade = Number(prompt('Qual a sua idade?'))
+let cartMortorista = prompt('Possui habilitação?')
+let posCarro = prompt('Possui carro?')
+
+if (idade < 18 || cartMortorista === "Não"){
+    console.log(nome+" ,você não pode dirigir.")
+}
+else if (idade > 18 || cartMortorista === "Sim" || posCarro == "Não"){
+    console.log(nome+" ,você pode dirigir, mas não tem carro.")
+}
+
+else if (idade > 18 || cartMortorista === "Sim" || posCarro == "Sim"){
+    console.log(nome+" ,você será o motorista!!")
 }
