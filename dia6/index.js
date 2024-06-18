3fd6591a4bd7f727d35b1b2ceda86a5bac8035b7
Deivide Costa <deividec51@gmail.com>
2024-06-18 08:53:57 -0300

    atividade tabuada a partir de um numero e dos seus dois seguintes

--- dia6/index.js
+++ dia6/index.js
@@ -1,5 +1,8 @@
-let numero = 0
-for(let i=50; i >= numero; i-=5){
-    console.log(i)
+let numero = 3
+
+for(let i = numero; i <= numero + 2 ; i++){
+  for(let contador = 0; contador <=10; contador++){
+    console.log(numero+"x"+contador+"="+numero*contador)
+  }
 }
 
